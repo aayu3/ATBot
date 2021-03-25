d873304521f34c804da98a0ba399e8c20da610d3
Aaron Yu <aaronyu3@illinois.edu>
2021-03-25 15:42:15 -0700

    Added ability to filter with multiple arguments.

--- app.js
+++ app.js
@@ -292,33 +292,56 @@ client.on('message', (msg) => {
 client.on('message', (msg) => {
   let messageContents = sanitizeCommand(msg);
   if (messageContents[0] == "filter") {
-    let lowered = messageContents[1].toLowerCase();
-    if (lowered === "ur" || lowered === "sr" || lowered == "r") {
-      let filtered = supporterSearch.filterByRarity(lowered, supporters);
-      let messages = supporterSearch.printMultiSupporters(filtered);
-      console.log(messages);
-      for (var i = 0; i < messages.length; i++) {
-        msg.channel.send(messages[i]);
-      }
-    } else if (lowered === "suppress" || lowered === "protect" || lowered == "assist") {
-      let filtered = supporterSearch.filterByType(lowered, supporters);
-      let messages = supporterSearch.printMultiSupporters(filtered);
-      console.log(messages);
-      for (var i = 0; i < messages.length; i++) {
-        msg.channel.send(messages[i]);
-      }
-    } else {
-      let filtered = supporterSearch.filterBySource(lowered, supporters);
-      if (filtered.length == 0) {
-        msg.reply("Please use a valid filter argument:\n**Rarity:**\nUR\nSR\nR\n**Type:**\nSuppress\nProtect\nAssist\nOr provide an source.")
-      } else {
+    // If there is only one argument
+    if (messageContents.length <= 1) {
+      msg.reply("Please provide an argument.");
+    } else if (messageContents.length == 2) {
+      let lowered = messageContents[1].toLowerCase();
+      if (lowered === "ur" || lowered === "sr" || lowered == "r") {
+        let filtered = supporterSearch.filterByRarity(lowered, supporters);
         let messages = supporterSearch.printMultiSupporters(filtered);
-        console.log(messages);
         for (var i = 0; i < messages.length; i++) {
           msg.channel.send(messages[i]);
         }
+      } else if (lowered === "suppress" || lowered === "protect" || lowered == "assist") {
+        let filtered = supporterSearch.filterByType(lowered, supporters);
+        let messages = supporterSearch.printMultiSupporters(filtered);
+        for (var i = 0; i < messages.length; i++) {
+          msg.channel.send(messages[i]);
+        }
+      } else {
+        let filtered = supporterSearch.filterBySource(lowered, supporters);
+        if (filtered.length == 0) {
+          msg.reply("Please use a valid filter argument:\n**Rarity:**\nUR\nSR\nR\n**Type:**\nSuppress\nProtect\nAssist\nOr provide an source.")
+        } else {
+          let messages = supporterSearch.printMultiSupporters(filtered);
+          for (var i = 0; i < messages.length; i++) {
+            msg.channel.send(messages[i]);
+          }
+        }
       }
-    }
+    } else if (messageContents.length > 4) {
+        msg.reply("There are too many arguments.")
+      } else {
+        let filtered = supporters;
+        for (var i = 1; i < messageContents.length; i++) {
+          let lowered = messageContents[i].toLowerCase();
+          if (lowered === "ur" || lowered === "sr" || lowered == "r") {
+            filtered = supporterSearch.filterByRarity(lowered, filtered);
+          } else if (lowered === "suppress" || lowered === "protect" || lowered == "assist") {
+            filtered = supporterSearch.filterByType(lowered, filtered);
+          } else {
+            filtered = supporterSearch.filterBySource(lowered, filtered);
+            if (filtered.length == 0) {
+              msg.reply("Please use a valid filter argument:\n**Rarity:**\nUR\nSR\nR\n**Type:**\nSuppress\nProtect\nAssist\nOr provide an source.")
+            } 
+          }
+        }
+        let messages = supporterSearch.printMultiSupporters(filtered);
+        for (var i = 0; i < messages.length; i++) {
+          msg.channel.send(messages[i]);
+        }
+    }  
   }
 });
 
