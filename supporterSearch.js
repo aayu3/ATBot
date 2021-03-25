b3d97efbd49c9a0d33705303b47513f8c8061ba2
Aaron Yu <aaronyu3@illinois.edu>
2021-03-24 21:35:53 -0700

    Fixed misspellings of suppress as well as succesfully reading json files into objects.

--- supporterSearch.js
+++ supporterSearch.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+
+// Declare emotes for formatting
 const suppressEmote = "<:suppress:824484622851112990>";
 const protectEmote = "<:protect:824484622544404501>";
 const assistEmote = "<:assist:824484622829355008>";
@@ -5,4 +8,26 @@ const rareEmote = "<:rare:824486578247172106>";
 const srEmote = "<:SR:824486914065301514>";
 const urEmote = "<:UR:824486578273255424>";
 
+// Setting up FS variables
+let supporterPath = "supporter_json";
+let supporterJSONs = fs.readdirSync(supporterPath);
 
+function parseJSON (folder) {
+    var supporters = [];
+    for (var i = 0; i < folder.length; i++) {
+        let rawdata = fs.readFileSync(supporterPath + "/" + folder[i]);
+        let sup = JSON.parse(rawdata);
+        supporters.push(sup);
+    }
+    return supporters;
+}
+
+function printSupporter(supporter) {
+    let strRepresentation = "**No. " + supporter.Number;
+    strRepresentation = strRepresentation + " " + supporter.Name + "** ";
+    if (supporter.Type === "")
+
+
+}
+
+parseJSON(supporterJSONs);
