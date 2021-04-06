28bfd3bcad046a19dc825b07ff3588520e466beb
Aaron Yu <67659126+aayu3@users.noreply.github.com>
2021-04-06 12:23:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -126,6 +126,14 @@ client.on('message', (msg) => {
   }
 });
 
+// list emote command
+client.on('message', (msg) => {
+  let messageContents = sanitizeCommand(msg);
+  if (messageContents[0] == "website") {
+    msg.reply("https://aayu3.github.io/ATBotJSONDependencies/");
+    }
+});
+
 //mute command
 client.on('message', (msg) => {
   let messageContents = sanitizeCommand(msg);
@@ -273,43 +281,48 @@ client.on('message', (msg) => {
 client.on('message', (msg) => {
   let messageContents = sanitizeCommand(msg);
   if (messageContents[0] == "search") {
-    if (isNaN(parseInt(messageContents[1]))) {
-      // In this case the person is searching by name
-      if (messageContents[1].toLowerCase() == "all") {
-        let all = supporterSearch.printMultiSupporters(supporters);
-        for (var i = 0; i < all.length; i++) {
-          msg.channel.send(all[i]);
+    if(messageContents.length > 1) {
+      if (isNaN(parseInt(messageContents[1]))) {
+        // In this case the person is searching by name
+        if (messageContents[1].toLowerCase() == "all") {
+          let all = supporterSearch.printMultiSupporters(supporters);
+          for (var i = 0; i < all.length; i++) {
+            msg.channel.send(all[i]);
+          }
+        } else {
+          let searchTerms = messageContents.shift();
+          let searchName = messageContents.join(" ");
+          let filteredSups = supporterSearch.filterByName(searchName, supporters);
+          for (var i = 0; i < filteredSups.length; i++) {
+          }
+          if (filteredSups.length == 0) {
+            msg.reply("There is no supporter with the name: " + searchName);
+          } else if (filteredSups.length == 1) {
+            let sup = filteredSups[0];
+            msg.channel.send(supporterSearch.printSupporter(sup));
+            msg.channel.send("https://aayu3.github.io/ATBotJSONDependencies/supporter_images/" + sup.Awakened + ".png");
+          } else {
+            let strings = supporterSearch.printMultiSupporters(filteredSups);
+            for (var i = 0; i < strings.length; i++) {
+              msg.channel.send(strings[i]);
+            }
+          }
         }
       } else {
-        let searchTerms = messageContents.shift();
-        let searchName = messageContents.join(" ");
-        let filteredSups = supporterSearch.filterByName(searchName, supporters);
-        for (var i = 0; i < filteredSups.length; i++) {
-        }
-        if (filteredSups.length == 0) {
-          msg.reply("There is no supporter with the name: " + searchName);
-        } else if (filteredSups.length == 1) {
-          let sup = filteredSups[0];
+        // In this case the person is searching by number
+        let num = parseInt(messageContents[1]) - 1;
+        if (num < 0 || num > supporters.length - 1) {
+          msg.reply("Please provide a number in the correct range: 1 - " + (supporters.length));
+        } else {
+          let sup = supporterSearch.searchByNumber(num + 1, supporters);
           msg.channel.send(supporterSearch.printSupporter(sup));
           msg.channel.send("https://aayu3.github.io/ATBotJSONDependencies/supporter_images/" + sup.Awakened + ".png");
-        } else {
-          let strings = supporterSearch.printMultiSupporters(filteredSups);
-          for (var i = 0; i < strings.length; i++) {
-            msg.channel.send(strings[i]);
-          }
         }
       }
     } else {
-      // In this case the person is searching by number
-      let num = parseInt(messageContents[1]) - 1;
-      if (num < 0 || num > supporters.length - 1) {
-        msg.reply("Please provide a number in the correct range: 1 - " + (supporters.length));
-      } else {
-        let sup = supporterSearch.searchByNumber(num + 1, supporters);
-        msg.channel.send(supporterSearch.printSupporter(sup));
-        msg.channel.send("https://aayu3.github.io/ATBotJSONDependencies/supporter_images/" + sup.Awakened + ".png");
-      }
+      msg.reply("Please provide a proper argument");
     }
+    
   }
 });
 
