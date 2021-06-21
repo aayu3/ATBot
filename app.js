2da5be4bbdc4728148293831d20bae4684552513
Aaron Yu <67659126+aayu3@users.noreply.github.com>
2021-06-20 22:13:26 -0700

    added weapon info to bot

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const supporterSearch = require("./supporterSearch.js");
+const weaponSearch = require("./weaponSearch.js")
 const request = require('request');
 const cheerio = require('cheerio');
 require('dotenv').config();
 
+const characters = ["igawa asagi", "igawa sakura", "mizuki yukikaze", "akiyama rinko", "mizuki shiranui", "yatsu murasaki", "emily simmons", "su jinglei", "koukawa oboro", "shinganji kurenai", "loukawa asuka", "onisaki kirara", "ingrid"];
 
 const url = "https://aayu3.github.io/ATBotJSONDependencies/supporters.json";
 let options = {json: true};
 
 
 var supporters = [];
+var weapons = [];
 request(url, options, (error, res, body) => {
     if (error) {
         return  console.log(error)
@@ -20,7 +23,17 @@ request(url, options, (error, res, body) => {
         supporters = body;
     };
 });
-console.log(supporters);
+
+const weaponurl = "https://aayu3.github.io/ATBotJSONDependencies/weapons.json";
+request(weaponurl, options, (error, res, body) => {
+  if (error) {
+      return  console.log(error)
+  };
+
+  if (!error && res.statusCode == 200) {
+      weapons = body;
+  };
+});
 //get files ready
 /*
 let rawdata = fs.readFileSync("supporters.json");
@@ -131,6 +144,13 @@ client.on('message', (msg) => {
   let messageContents = sanitizeCommand(msg);
   if (messageContents[0] == "website") {
     msg.reply("https://aayu3.github.io/ATBotJSONDependencies/");
+    }
+});
+
+client.on('message', (msg) => {
+  let messageContents = sanitizeCommand(msg);
+  if (messageContents[0] == "commands") {
+    msg.reply("__**Supporters**__\n!supporter [Name/Number]\n!filtersup [Rarity/Status/Type/Source]\n!intimacy [Name]\n__**Weapons**__\n!weapon [Name/Number]\n!filterwep [Rarity/Source]\n!weaponchar [Character]\n__**Guides**__\n!guide (For Beginner's Guide)\n!reroll (For Reroll Guide)");
     }
 });
 
@@ -285,10 +305,47 @@ client.on('message', (msg) => {
   }
 });
 
+// intimacy command
+client.on('message', (msg) => {
+  let messageContents = sanitizeCommand(msg);
+  if (messageContents[0] == "intimacy") {
+  
+    if(messageContents.length > 1) {
+      if (isNaN(parseInt(messageContents[1]))) {
+        // In this case the person is searching by name
+        let searchName = messageContents.slice(1).join(" ");
+        let filteredSups = supporterSearch.filterByName(searchName, supporters);
+        if (filteredSups.length == 0) {
+          msg.reply("There is no supporter with the name: " + searchName);
+        } else if (filteredSups.length == 1) {
+          let sup = filteredSups[0];
+          msg.channel.send("https://aayu3.github.io/ATBotJSONDependencies/intimacy_images/Supporter_" + sup.Unawakened + ".png");
+        } else {
+          msg.reply("Please be more specific")
+        } 
+      }
+      else {
+        // In this case the person is searching by number
+        let num = parseInt(messageContents[1]) - 1;
+        if (num < 0 || num > supporters.length - 1) {
+          msg.reply("Please provide a number in the correct range: 1 - " + (supporters.length));
+        } else {
+          let sup = supporterSearch.searchByNumber(num + 1, supporters);
+          msg.channel.send("https://aayu3.github.io/ATBotJSONDependencies/intimacy_images/Supporter_" + sup.Unawakened + ".png");
+        }
+      }
+      
+    }
+    else {
+      msg.reply("Please provide an argument.")
+    }
+  } 
+});
+
 // Search by number or name
 client.on('message', (msg) => {
   let messageContents = sanitizeCommand(msg);
-  if (messageContents[0] == "search") {
+  if (messageContents[0] == "supporter") {
     if(messageContents.length > 1) {
       if (isNaN(parseInt(messageContents[1]))) {
         // In this case the person is searching by name
@@ -301,8 +358,6 @@ client.on('message', (msg) => {
           let searchTerms = messageContents.shift();
           let searchName = messageContents.join(" ");
           let filteredSups = supporterSearch.filterByName(searchName, supporters);
-          for (var i = 0; i < filteredSups.length; i++) {
-          }
           if (filteredSups.length == 0) {
             msg.reply("There is no supporter with the name: " + searchName);
           } else if (filteredSups.length == 1) {
@@ -337,7 +392,7 @@ client.on('message', (msg) => {
 // filter by source, type, or rarity
 client.on('message', (msg) => {
   let messageContents = sanitizeCommand(msg);
-  if (messageContents[0] == "filter") {
+  if (messageContents[0] == "filtersup") {
     // If there is only one argument
     if (messageContents.length <= 1) {
       msg.reply("Please provide an argument.");
@@ -346,8 +401,7 @@ client.on('message', (msg) => {
       if (lowered === "ur" || lowered === "sr" || lowered == "r") {
         let filtered = supporterSearch.filterByRarity(lowered, supporters);
         let messages = supporterSearch.printMultiSupporters(filtered);
-        console.log("messages: ");
-        console.log(messages);
+
         if (messages[0] === "") {
           msg.channel.send("There are no supporters that fit these requirements.");
         } else {
@@ -359,8 +413,7 @@ client.on('message', (msg) => {
       } else if (lowered === "suppress" || lowered === "protect" || lowered == "assist") {
         let filtered = supporterSearch.filterByType(lowered, supporters);
         let messages = supporterSearch.printMultiSupporters(filtered);
-        console.log("messages: ");
-        console.log(messages);
+
         if (messages[0] === "") {
           msg.channel.send("There are no supporters that fit these requirements.");
         } else {
@@ -372,8 +425,8 @@ client.on('message', (msg) => {
       } else if (lowered === "released" || lowered === "unreleased" ) {
         let filtered = supporterSearch.filterByStatus(lowered, supporters);
         let messages = supporterSearch.printMultiSupporters(filtered);
-        console.log("messages: ");
-        console.log(messages);
+
+
         if (messages[0] === "") {
           msg.channel.send("There are no supporters that fit these requirements.");
         } else {
@@ -417,12 +470,12 @@ client.on('message', (msg) => {
           }
         }
         let messages = supporterSearch.printMultiSupporters(filtered);
-        console.log("messages: ");
-        console.log(messages);
+
         if (messages[0] === "") {
           msg.channel.send("There are no supporters that fit these requirements.");
         } else {
-          console.log(messages);
+
+
           for (var i = 0; i < messages.length; i++) {
             if (messages[i].length == 0) {
               msg.channel.send("There are no supporters that fit these requirements.");
@@ -435,5 +488,162 @@ client.on('message', (msg) => {
   }
 });
 
+// Search weapon by number or name
+client.on('message', (msg) => {
+  let messageContents = sanitizeCommand(msg);
+  if (messageContents[0] == "weapon") {
+    if(messageContents.length > 1) {
+      if (isNaN(parseInt(messageContents[1]))) {
+        // In this case the person is searching by name
+        if (messageContents[1].toLowerCase() == "all") {
+          let all = weaponSearch.printMultiWeapons(weapons);
+          for (var i = 0; i < all.length; i++) {
+            msg.channel.send(all[i]);
+          }
+        } else {
+          let searchTerms = messageContents.shift();
+          let searchName = messageContents.join(" ");
+          console.log(searchName);
+          let filteredWeps = weaponSearch.filterByName(searchName, weapons);
+          if (filteredWeps.length == 0) {
+            msg.reply("There is no weapon with the name: " + searchName);
+          } else if (filteredWeps.length == 1) {
+            let wep = filteredWeps[0];
+            msg.channel.send(weaponSearch.printWeapon(wep));
+            msg.channel.send("https://aayu3.github.io/ATBotJSONDependencies/weapon_images/" + wep.Icon);
+          } else {
+            let strings = weaponSearch.printMultiWeapons(filteredWeps);
+            for (var i = 0; i < strings.length; i++) {
+              msg.channel.send(strings[i]);
+            }
+          }
+        }
+      } else {
+        // In this case the person is searching by number
+        let num = parseInt(messageContents[1]) - 1;
+        if (num < 0 || num > weapons.length - 1) {
+          msg.reply("Please provide a number in the correct range: 1 - " + (weapons.length));
+        } else {
+          let wep = weaponSearch.searchByNumber(num + 1, weapons);
+          msg.channel.send(weaponSearch.printWeapon(wep));
+          msg.channel.send("https://aayu3.github.io/ATBotJSONDependencies/weapon_images/" + wep.Icon);
+        }
+      }
+    } else {
+      msg.reply("Please provide a proper argument");
+    }
+    
+  }
+});
 
+// filter by source, type, or rarity
+client.on('message', (msg) => {
+  let messageContents = sanitizeCommand(msg);
+  if (messageContents[0] == "filterwep") {
+    // If there is only one argument
+    if (messageContents.length <= 1) {
+      msg.reply("Please provide an argument.");
+    } else if (messageContents.length == 2) {
+      let lowered = messageContents[1].toLowerCase();
+      if (lowered === "ur" || lowered === "sr" || lowered == "rare" || lowered == 'normal') {
+        let filtered = weaponSearch.filterByRarity(lowered, weapons);
+        let messages = weaponSearch.printMultiWeapons(filtered);
+
+        if (messages[0] === "") {
+          msg.channel.send("There are no weapons that fit these requirements.");
+        } else {
+        for (var i = 0; i < messages.length; i++) {
+        
+          msg.channel.send(messages[i]);
+        }
+      }
+      } else if (characters.includes(lowered)) {
+        let filtered = weaponSearch.filterByCharacter(lowered, weapons);
+        let messages = weaponSearch.printMultiSupporters(filtered);
+
+        if (messages[0] === "") {
+          msg.channel.send("There are no weapons that fit these requirements.");
+        } else {
+          
+        for (var i = 0; i < messages.length; i++) {
+          msg.channel.send(messages[i]);
+        }
+      }
+      } else {
+        let filtered = weaponSearch.filterBySource(lowered, weapons);
+        if (filtered.length == 0) {
+          msg.reply("Please use a valid filter argument:\n**Rarity:**\nUR\nSR\nR\nN\nOr provide an source.")
+        } else {
+          let messages = weaponSearch.printMultiWeapons(filtered);
+          if (messages[0] === "") {
+            msg.channel.send("There are no weapons that fit these requirements.");
+          } else {
+          for (var i = 0; i < messages.length; i++) {
+            msg.channel.send(messages[i]);
+          }
+        }
+        }
+      }
+    } else if (messageContents.length > 3) {
+        msg.reply("There are too many arguments.")
+      } else {
+        let filtered = weapons;
+        for (var i = 1; i < messageContents.length; i++) {
+          let lowered = messageContents[i].toLowerCase();
+          if (lowered === "ur" || lowered === "sr" || lowered == "rare" || lowered === 'normal') {
+            filtered = weaponSearch.filterByRarity(lowered, filtered);
+          } else {
+            filtered = weaponSearch.filterBySource(lowered, filtered);
+            if (filtered.length == 0) {
+              msg.reply("Please use a valid filter argument:\n**Rarity:**\nUR\nSR\nR\nN\nOr provide an source.")
+            } 
+          }
+        }
+        let messages = weaponSearch.printMultiWeapons(filtered);
+
+        if (messages[0] === "") {
+          msg.channel.send("There are no weapons that fit these requirements.");
+        } else {
+
+
+          for (var i = 0; i < messages.length; i++) {
+            if (messages[i].length == 0) {
+              msg.channel.send("There are no weapons that fit these requirements.");
+            }
+            msg.channel.send(messages[i]);
+          }
+        }
+        
+    }  
+  }
+});
+
+
+client.on('message', (msg) => {
+  let messageContents = sanitizeCommand(msg);
+  if (messageContents[0] == "weaponchar") {
+    // If there is only one argument
+    if (messageContents.length <= 1) {
+      msg.reply("Please provide an argument.");
+    } else {
+      let supporterName = messageContents.slice(1);
+      let lowered = supporterName.join(" ").toLowerCase();
+      console.log(lowered);
+        filtered = weaponSearch.filterByCharacter(lowered, weapons);
+      if (filtered.length > 1) {
+        let messages = weaponSearch.printMultiWeapons(filtered);
+        for (var i = 0; i < messages.length; i++) {
+          msg.channel.send(messages[i]);
+        }
+      } else if (filtered.length == 1) {
+        msg.channel.send(weaponSearch.printWeapon(filtered[0]));
+      } else {
+        msg.channel.send("There are no weapons that fit this category.");
+      }
+      
+      
+
+    } 
+  }
+});
 // Token Change
---
+++ weaponSearch.js
@@ -0,0 +1,174 @@
+const fs = require('fs');
+const request = require('request');
+
+const url = "https://aayu3.github.io/ATBotJSONDependencies/weapons.json";
+
+let options = {json: true};
+
+
+let wep;
+request(url, options, (error, res, body) => {
+    if (error) {
+        return  console.log(error)
+    };
+
+    if (!error && res.statusCode == 200) {
+        wep = body;
+        console.log(wep.Name);
+    };
+});
+console.log(wep);
+// Declare emotes for formatting
+
+const rareEmote = "<:rare:824486578247172106>";
+const srEmote = "<:SR:824486914065301514>";
+const urEmote = "<:UR:824486578273255424>";
+const normalEmote = "<:normal:853495141541347328>";
+const particleEmote = "<:particle:853495036175843369>"
+
+// Setting up FS variables
+
+module.exports = {
+// parses all the files in the folder into JSON objects
+    // prints supporter info before the image
+    printWeapon: function (weapon) {
+        // Name and Rarity Formatting
+        let strRep = "**No. " + weapon.Number;
+        strRep = strRep + " __" + weapon.Name + "__** ";
+        strRep = strRep + "\n**Source:** " + weapon.Source + "\n**Rarity:** ";
+        if (weapon.Rarity === "Normal") {
+            strRep = strRep + normalEmote;
+        }
+        else if (weapon.Rarity === "Rare") {
+            strRep = strRep + rareEmote;
+        } else if (weapon.Rarity == "SR") {
+            strRep = strRep + srEmote;
+        } else if (weapon.Rarity == "UR") {
+            strRep = strRep + urEmote;
+        }
+        strRep = strRep + "\n**Character:** " + weapon.Character;
+        // Source Formatting
+        strRep = strRep + "\n**Source:** " + weapon.Source;
+
+        // Attack and Crit Formatting
+        /*"MaxForgeAttack":504,
+          "MaxForgeCrit":504,
+          "MaxSLVLAttack":756,
+          "MaxSLVLCrit":756, */
+        strRep = strRep + "\n**Maximum Forge Attack: **" + weapon.MaxForgeAttack;
+        strRep = strRep + "\n**Maximum Forge Crit: **" + weapon.MaxForgeCrit;
+        strRep = strRep + "\n**Maximum SLVL Attack: **" + weapon.MaxSLVLAttack;
+        strRep = strRep + "\n**Maximum SLVL Crit: **" + weapon.MaxSLVLCrit;
+
+        // Check for Main Skill then format it
+        if (weapon.SkillDescription) {
+            let description = weapon.SkillDescription;
+            if (description.split("N/A").length > 1) {
+                return strRep;
+            } else {
+                strRep = strRep + "\n**Skill Name: **" + weapon.SkillName;
+                if (weapon.Particle != -1) {
+                    strRep = strRep + "\n**Particle Cost: **" + particleEmote + " x " + Math.floor(weapon.Particle/100);
+                }
+                description = description.replace(/ *\[[^\]]*]/g, ' ');
+                strRep = strRep + "\n**Skill Description: **" + description;
+
+            }
+        }
+
+        
+        return strRep
+    },
+
+    printMultiWeapons: function(weapons) {
+        let strs = [];
+        str = "";
+        for (var i = 0; i < weapons.length; i++) {
+            if (str.length <= 2000 && str.length >= 1900) {
+                str = str.substring(0, str.length - 1);
+                strs.push(str);
+                str = "";
+            }
+            let weapon = weapons[i];
+            // add rarity at front
+            if (weapon.Rarity === "Normal") {
+                str = str + normalEmote;
+            }
+            else if (weapon.Rarity === "Rare") {
+                str = str + rareEmote;
+            } else if (weapon.Rarity == "SR") {
+                str = str + srEmote;
+            } else if (weapon.Rarity == "UR") {
+                str = str + urEmote;
+            }
+
+            // add name
+            str = str + " **" + weapon.Name + "** ";
+
+            // add type
+           str = str + weapon.Character;
+            str = str + "\n";
+        }
+        strs.push(str);
+        return strs;
+    },
+
+    // assumes the input is legal
+    searchByNumber: function (num, supporters) {
+        return supporters[num-1];
+    },
+
+    filterByName: function(str, supporters) {
+        let filtered = [];
+        for (var i = 0; i < supporters.length; i++) {
+            let sup = supporters[i];
+            if (sup.hasOwnProperty("Name")) {
+                if (sup.Name.toLowerCase().includes(str.toLowerCase())) {
+                    filtered.push(sup);
+                }
+            }
+        }
+        return filtered;
+    },
+
+
+
+    filterByRarity: function(rarity, supporters) {
+        let filtered = [];
+        for (var i = 0; i < supporters.length; i++) {
+            let sup = supporters[i]; 
+            if (sup.hasOwnProperty("Rarity")) {
+            if (sup.Rarity.toLowerCase() === rarity.toLowerCase()) {
+                filtered.push(sup);
+            }
+        }
+        }
+        return filtered;
+    },
+
+    filterByCharacter: function (character, supporters) {
+        let filtered = [];
+        for (var i = 0; i < supporters.length; i++) {
+            let sup = supporters[i];
+            if (sup.hasOwnProperty("Character")) {
+            if (sup.Character.toLowerCase().includes(character.toLowerCase())) {
+                filtered.push(sup);
+            }
+        }
+        }
+        return filtered;
+    },
+    
+    filterBySource: function (source, supporters) {
+        let filtered = [];
+        for (var i = 0; i < supporters.length; i++) {
+            let sup = supporters[i];
+            if (sup.hasOwnProperty("Source")) {
+            if (sup.Source.toLowerCase().includes(source.toLowerCase())) {
+                filtered.push(sup);
+            }
+        }
+        }
+        return filtered;
+    }
+}
